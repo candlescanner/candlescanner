452de2d4d0b99341852741ea7186094c03ec68b5
jon <noshoulders@gmail.com>
2018-07-27 10:32:57 +0100

    oops

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ wsurl = "wss://testnet.bitmex.com/realtime";
 
 subs = '{"op": "subscribe", "args": ["quote", "tradeBin1m", "tradeBin5m", "tradeBin1h", "tradeBin1d"]}';
 
-var ws = new WebSocket("wsurl");
+var ws = new WebSocket(wsurl);
 
 ws.onopen = function (event) {
     ws.send(subs)
